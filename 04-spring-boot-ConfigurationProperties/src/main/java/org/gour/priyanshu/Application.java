65cca9c69085a305f82c40a79ae2663ab96dd096
Priyanshu Gour <priyanshugour1@gmail.com>
2024-06-22 11:11:18 +0530

    ConfigurationProperties All Types Completed

--- 04-spring-boot-ConfigurationProperties/src/main/java/org/gour/priyanshu/Application.java
+++ 04-spring-boot-ConfigurationProperties/src/main/java/org/gour/priyanshu/Application.java
@@ -1,13 +1,24 @@
 package org.gour.priyanshu;
 
+import org.gour.priyanshu.beans.Company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		// Start the container
+		System.out.println("***********Container Started****************");
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+
+		Company company = context.getBean(Company.class);
+		System.out.println(company);
+
+		// closing the container
+		((ConfigurableApplicationContext) context).close();
+		System.out.println("***********Container Stopped****************");
 	}
 
 }
--- 05-spring-boot-ConfigurationProperties-AllType/src/main/java/org/gour/priyanshu/Application.java
+++ 05-spring-boot-ConfigurationProperties-AllType/src/main/java/org/gour/priyanshu/Application.java
@@ -1,13 +1,31 @@
 package org.gour.priyanshu;
 
+import org.gour.priyanshu.beans.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		// Start the container
+		System.out.println("***********Container Started****************");
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+
+		Employee employee = context.getBean(Employee.class);
+		System.out.println(employee);
+
+		DataSource source = context.getBean(DataSource.class);
+		System.out.println("DataSource is :: "+source.getClass().getName());
+
+		System.out.println();
+
+		// closing the container
+		((ConfigurableApplicationContext) context).close();
+		System.out.println("***********Container Stopped****************");
 	}
 
 }
