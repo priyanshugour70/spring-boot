0e565137212d13541af2d996a99468781bd8d7e2
Priyanshu Gour <priyanshugour1@gmail.com>
2024-07-13 15:30:00 +0530

    SpringBoot-JPA-Repository-OneToMany

---
+++ 29-spring-boot-jparepository-OneToManyEx-uni/src/main/java/com/gour/priyanshu/dao/IModelRepository.java
@@ -0,0 +1,8 @@
+package com.gour.priyanshu.dao;
+
+import com.gour.priyanshu.entity.Model;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IModelRepository extends JpaRepository<Model, Integer> {
+
+}
