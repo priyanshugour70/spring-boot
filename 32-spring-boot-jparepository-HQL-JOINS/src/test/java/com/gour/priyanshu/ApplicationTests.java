4fd633da0839f4aedc0319a0bace5f9decfb1595
Priyanshu Gour <priyanshugour1@gmail.com>
2024-07-16 15:30:00 +0530

    SpringBoot-JPA-Repository-HQL-JOINS

---
+++ 32-spring-boot-jparepository-HQL-JOINS/src/test/java/com/gour/priyanshu/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gour.priyanshu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
