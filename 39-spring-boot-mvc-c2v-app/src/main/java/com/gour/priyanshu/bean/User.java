7cd8c1c8f433e815ce535ec6924b37ef684e177a
Priyanshu Gour <priyanshugour1@gmail.com>
2024-07-28 12:15:17 +0530

    Spring-boot-mvc-Controller to View

---
+++ 39-spring-boot-mvc-c2v-app/src/main/java/com/gour/priyanshu/bean/User.java
@@ -0,0 +1,25 @@
+package com.gour.priyanshu.bean;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private Integer userId;
+    private String userName;
+    private String userRole;
+
+}
+
+
+
+
