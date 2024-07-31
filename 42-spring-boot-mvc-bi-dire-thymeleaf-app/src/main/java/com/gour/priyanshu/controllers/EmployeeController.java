0f0d061a2407160ec6a56afab7934033e91831f7
Priyanshu Gour <priyanshugour1@gmail.com>
2024-07-31 14:45:00 +0530

    MVC bi-dire thymeleaf

---
+++ 42-spring-boot-mvc-bi-dire-thymeleaf-app/src/main/java/com/gour/priyanshu/controllers/EmployeeController.java
@@ -0,0 +1,48 @@
+package com.gour.priyanshu.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gour.priyanshu.bean.Employee;
+
+@Controller
+@RequestMapping(value = "/employee")
+public class EmployeeController {
+
+    /***
+     * 1. METHOD TYPE :: GET
+     * 2. INPUT       :: @ModelAttribute,Model
+     * 3. PATH        :: /showForm
+     * 4. R.T         :: EmployeeForm
+     *
+     */
+    @GetMapping(value = "/showForm")
+    public String showForm(@ModelAttribute  Employee employee,
+                           Model model
+    ) {
+        model.addAttribute("employee", employee);
+        return "EmployeeForm";
+    }
+
+    /***
+     * 1. METHOD TYPE :: POST
+     * 2. INPUT       :: @ModelAttribute Model
+     * 3. PATH        :: /save
+     * 4. R.T         :: EmployeeForm
+     *
+     */
+    @PostMapping("/save")
+    public String saveFormData(
+            @ModelAttribute
+            Employee employee,
+            Model model) {
+
+        model.addAttribute("employee", employee);
+
+        return "EmployeeForm";
+    }
+}
