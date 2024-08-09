d077d818de136e3f4f6b5d83f3645504a08fa9a1
Priyanshu Gour <priyanshugour1@gmail.com>
2024-08-09 13:13:13 +0530

    spring Rest Response Entity App

---
+++ 47-spring-boot-springResponseEntityApp/src/main/java/com/gour/priyanshu/bean/Role.java
@@ -0,0 +1,14 @@
+package com.gour.priyanshu.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    private Integer roleId;
+    private String roleCode;
+
+}
