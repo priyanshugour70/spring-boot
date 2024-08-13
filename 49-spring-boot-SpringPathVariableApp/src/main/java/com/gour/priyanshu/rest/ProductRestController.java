695c875657f0443fadf5ea1381550d04fe2f0817
Priyanshu Gour <priyanshugour1@gmail.com>
2024-08-13 13:13:13 +0530

    spring Rest Spring path Variable App

--- 49-spring-boot-SpringPathVariableApp/src/main/java/com/gour/priyanshu/rest/ProductRestController.java
+++ 49-spring-boot-SpringPathVariableApp/src/main/java/com/gour/priyanshu/rest/ProductRestController.java
@@ -1,11 +1,19 @@
 package com.gour.priyanshu.rest;
 
+import java.util.Enumeration;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/v1/api/product")
@@ -52,6 +60,57 @@ public class ProductRestController {
         String body = "PID = "+pid + " : "+"PNAME = "+pname;
         return ResponseEntity.ok(body);
     }
+
+
+
+    /*
+     * R.T    : ResponseEntity<String>
+     * METHOD : GET
+     * PATH   : /load -> static path
+     * INPUT  : No Input
+     *
+     */
+    @GetMapping("/load")
+    public ResponseEntity<String> getHeaderInfo(
+            @RequestHeader	Map<String,String> headers
+    ) {
+
+        headers.forEach((key, value) -> {
+            System.out.println(String.format("Headers  '%s' = %s", key, value));
+        });
+
+        return new ResponseEntity<>(String.format("Listed  %d Headers ",headers.size()),HttpStatus.OK);
+    }
+
+    /*
+     * R.T    : ResponseEntity<String>
+     * METHOD : GET
+     * PATH   : /info -> static path
+     * INPUT  : No Input
+     *
+     */
+    @GetMapping("/info")
+    public ResponseEntity<String> getHeaderDetails(
+            HttpServletRequest request
+    ) {
+
+        Enumeration<String> names = request.getHeaderNames();
+        String name = "";
+        String value = "";
+        String body = "";
+        while (names.hasMoreElements()) {
+            name = (String) names.nextElement();
+            value = request.getHeader(name);
+            System.out.println(name + ": " + value);
+            body+=name + ": "+value +"\n";
+        }
+        return  new ResponseEntity<String>(body,HttpStatus.OK);
+    }
+
+
+
+
+
 
 
 
